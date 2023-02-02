2c2e8c3df8f1c014c68921f861b3c246d5785af8
Sha-94 <70490210+Sha-94@users.noreply.github.com>
2023-02-02 12:30:30 -0600

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { AdminModule } from './admin/admin.module';
 
 import { AppComponent } from './app.component';
+import { DonutListComponent } from './admin/containers/donut-list/donut-list.component';
+import { DonutSingleComponent } from './admin/containers/donut-single/donut-single.component';
+
+const ROUTES: Routes = [
+  {path: 'donuts', component:DonutListComponent},
+  {path: 'donut', component: DonutSingleComponent},
+  {path: '', pathMatch:'full',  redirectTo:'donuts'}
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AdminModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    AdminModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
